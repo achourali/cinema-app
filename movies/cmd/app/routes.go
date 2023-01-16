e41f55bf39884fcdc8691a4bf4ee86f5690261e3
achourali <achourali1999@gmail.com>
2023-01-16 13:41:27 +0100

    adding health check to movies microservice

--- movies/cmd/app/routes.go
+++ movies/cmd/app/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() *mux.Router {
 	r.HandleFunc("/api/movies/{id}", app.findByID).Methods("GET")
 	r.HandleFunc("/api/movies/", app.insert).Methods("POST")
 	r.HandleFunc("/api/movies/{id}", app.delete).Methods("DELETE")
+	r.HandleFunc("/healthz/", app.healthz).Methods("GET")
 
 	return r
 }
